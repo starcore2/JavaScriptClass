4258c79ed4e95cd91510295619b7ac02993ac952
starcore2 <liam@zacher.us>
2022-02-12 20:24:07 -0600

    lets start with the comments

---
+++ week6/scripts/task6.js
@@ -0,0 +1,23 @@
+//Guess that pokemon
+//First we need to pick a random pokemon
+//We need to have information requests
+//A basic html to display what is known
+//reset button to pick a new pokemon
+//differnt types of input to see what you want to know.
+/*
+    The core idea of differnt inputs is we can ask questions, the less questions asked, the better your score will be.
+    What questions to ask is
+    What is one of it's stats (attack, defence, special, etc.)
+    What is one of it's abilties can it have? counts as two questions 
+    What item does it tend to hold
+    What is a move does it know? counts as three questions
+    How many moves can it know? move pool quanty?
+    Does it have one or two types? 
+    What type is it? counts as 3 questions
+    Each failed guess will count as a question.
+*/
+
+function reset()
+{
+
+}
